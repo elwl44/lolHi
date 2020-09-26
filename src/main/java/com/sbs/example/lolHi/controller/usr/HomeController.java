0f3bb9389dbc9415b33ee0784936771840be7676
elwl44 <pbk11908@gmail.com>
2020-09-27 01:21:19 +0900

    게시물 리스트 기초 구현복습

--- src/main/java/com/sbs/example/lolHi/controller/usr/HomeController.java
+++ src/main/java/com/sbs/example/lolHi/controller/usr/HomeController.java
@@ -1,48 +1,56 @@
 package com.sbs.example.lolHi.controller.usr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbs.example.lolHi.service.ArticleService;
+	
+
+	
 @Controller
 public class HomeController {
-	@RequestMapping("/user/home/main")
+	@Autowired
+	private ArticleService articleService;
+	
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕";
 	}
 
-	@RequestMapping("/user/home/main2")
+	@RequestMapping("/usr/home/main2")
 	@ResponseBody
 	public String showMain2() {
 		return "잘가";
 	}
 
-	@RequestMapping("/user/home/main3")
+	@RequestMapping("/usr/home/main3")
 	@ResponseBody
 	public String showMain3() {
 		return "또봐";
 	}
 
-	@RequestMapping("/user/home/plus")
+	@RequestMapping("/usr/home/plus")
 	@ResponseBody
 	public int showPlus(int a, int b) {
 		return a + b;
 	}
 
-	@RequestMapping("/user/home/min")
+	@RequestMapping("/usr/home/min")
 	@ResponseBody
 	public int showmin(int a, int b) {
 		return a - b;
 	}
 
-	@RequestMapping("/user/home/mul")
+	@RequestMapping("/usr/home/mul")
 	@ResponseBody
 	public int showmul(int a, int b) {
 		return a * b;
 	}
 
-	@RequestMapping("/user/home/div")
+	@RequestMapping("/usr/home/div")
 	@ResponseBody
 	public int showdiv(int a, int b) {
 		return a / b;
